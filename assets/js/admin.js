428ffb7235feec3449edb791f77a8925d645108a
souqgsm-coder <souqgsm@gmail.com>
2026-06-10 14:47:02 +0100

    Add admin JavaScript

---
+++ assets/js/admin.js
@@ -0,0 +1,6 @@
+// Admin JavaScript for COURTIQ CTQ
+(function() {
+  'use strict';
+  const cfg = window.CTQ_ADMIN || {};
+  console.log('%c🌐 CTQ Admin v' + cfg.version, 'color:#1e3d89;font-weight:700');
+})();
